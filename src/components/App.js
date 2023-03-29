3d29aee16f4f9b6df29734bf3b878c6b92f9ce9c
YohannHERBET <yohannherbet.pro@gmail.com>
2023-03-29 17:41:18 +0200

    footer

--- src/components/App.js
+++ src/components/App.js
@@ -1,23 +1,25 @@
 import '../styles/index.scss';
 import 'bootstrap/dist/css/bootstrap.min.css';
 import Header from './Header';
+import Footer from './Footer';
 import ProjetSection from './ProjectSection';
 import AProposSection from './AProposSection';
 import TechnoSection from './TechnoSection';
 import ScrollToTopButton from './ScrollToTopButton';
 import ContactForm from './ContactForm';
 
-function App() {
-  return (
-    <div className="App space-background">
-      <Header />
+const App = () => (
+  <div className="App space-background">
+    <Header />
+    <main>
       <AProposSection />
       <TechnoSection />
       <ProjetSection />
       <ScrollToTopButton />
       <ContactForm />
-    </div>
-  );
-}
+    </main>
+    <Footer />
+  </div>
+);
 
 export default App;
